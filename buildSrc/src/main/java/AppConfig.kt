9da6af79203ff6fc87f3e51a76966d0582a4d0e0
최민재 <cmjcmj1230@gmail.com>
2023-06-17 20:22:47 +0900

    ⚡️ :: Add BuildSrc

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,24 @@
+import org.gradle.api.JavaVersion
+
+object AppConfig {
+    const val appNamespace = "com.mooooong.melog"
+    const val compileSdk = 33
+    const val applicationId = "com.mooooong.melog"
+    const val minSdk = 26
+    const val targetSdk = 33
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+
+    const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
+    const val useSupportLibrary = true
+    const val buildTypeName = "release"
+    const val minifyEnabled = false
+    const val defaultProguardFile = "proguard-android-optimize.txt"
+    const val proguardConsumerRules =  "consumer-rules.pro"
+
+    @JvmField val javaVersion = JavaVersion.VERSION_1_8
+    const val jvmTarget = "1.8"
+    const val compose = true
+    const val kotlinCompilerExtensionVersion = "1.3.2"
+    const val packagingOption = "/META-INF/{AL2.0,LGPL2.1}"
+}
---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,5 @@
+object Plugins {
+    const val application = "com.android.application"
+    const val library = "com.android.library"
+    const val kotlinAndroid = "org.jetbrains.kotlin.android"
+}
